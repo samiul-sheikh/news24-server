2e1608f6058e8789698175bfdca418bf3ece0576
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-09-08 02:29:18 +0600

    connected mongodb successfully

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const app = express()
-const port = 8000
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config()
+
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASS);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2uohe.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const port = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const collection = client.db("test").collection("articles");
+    console.log("database connected successfully!")
+});
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Welcome to News24 Server!')
 })
 
 app.listen(port, () => {
