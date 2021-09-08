611c7144e423abe129fc28d14c6e241d1e5bb57f
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-09-08 15:53:57 +0600

    add news information in server

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config()
 
-// console.log(process.env.DB_USER);
-// console.log(process.env.DB_PASS);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2uohe.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const port = process.env.PORT || 8000;
 
@@ -15,14 +13,22 @@ app.use(bodyParser.json());
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    const collection = client.db("test").collection("articles");
     console.log("database connected successfully!")
+    const newsCollection = client.db("news24").collection("news");
+
+    app.get('/', (req, res) => {
+        res.send('Welcome to News24 Server!')
+    })
+
+    // add news in server
+    app.post('/addNews', (req, res) => {
+        const newNews = req.body;
+        newsCollection.insertOne(newNews)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
 });
-
-
-app.get('/', (req, res) => {
-    res.send('Welcome to News24 Server!')
-})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
